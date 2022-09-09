b1554401510c3e223c52582c4657f78b413307b5
Manuel Goulão <msilvagoulao@gmail.com>
2022-09-09 17:42:50 +0100

    v 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='Sphinx Versioning for Github Pages',
-      version='0.0.1',
+      version='0.0.2',
       description='',
       author='Manuel Goulão',
       author_email='',
